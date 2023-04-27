27033b423e718f8e5d6576a8fa5cf0fa5d409405
Benjamin Faure <bfaure@z4r6p5.42lyon.fr>
2023-04-27 18:12:52 +0200

    change error display

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bfaure <bfaure@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:52:03 by bfaure            #+#    #+#             */
-/*   Updated: 2023/04/26 16:38:44 by bfaure           ###   ########lyon.fr   */
+/*   Updated: 2023/04/27 15:16:38 by bfaure           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	main(int argc, char **argv)
 
 	lst_b = NULL;
 	if (join_arg(argc, argv, &data) == -1)
-		return (ft_putstr_fd("Error\n", 2), -1);
+		return (-1);
 	if (split_args(&data) == -1)
-		return (ft_putstr_fd("Error\n", 2), -1);
+		return (-1);
 	parsing_error(&data, &lst_a, 0);
 	lst_a = make_list_a(&data);
 	if (!lst_a)
